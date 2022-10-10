9c85797fc6e8bc52ad0c36451c15853fb6248f08
gaffneylg <gaffneylg@gmail.com>
2022-10-10 18:00:38 +0100

    multiple request handling with pattern matching

--- lib/servy/handler.ex
+++ lib/servy/handler.ex
@@ -13,16 +13,34 @@ defmodule Servy.Handler do
         |> String.split("\n")
         |> List.first()
         |> String.split(" ")
-    %{method: method, path: path, resp_body: ""}
+    %{method: method, path: path, resp_body: "", status: nil}
   end
 
   def route(conv) do
-    %{ conv | resp_body: "Bears, Lions, Tigers"}
+    route(conv, conv.method, conv.path)
+  end
+
+
+  def route(conv, "GET", "/wildthings") do
+    %{ conv | resp_body: "Bears, Lions, Tigers", status: 200}
+  end
+
+  def route(conv, "GET", "/bears") do
+    %{ conv | resp_body: "Teddy, Paddington, Yogi", status: 200}
+  end
+
+  def route(conv, "GET", "/bears/" <> id) do
+    %{ conv | resp_body: "Bear #{id}", status: 200}
+  end
+
+  def route(conv, method, path) do
+    IO.inspect("No function clause matching method type #{method} with path #{path}.")
+    %{ conv | resp_body: "#{path} is an invalid path.", status: 404}
   end
 
   def format_response(conv) do
     """
-    HTTP/1.1 200 OK
+    HTTP/1.1 #{conv.status} #{status_reason(conv.status)}
     Content-Type: text/html
     Content-Length: #{String.length(conv.resp_body)}
 
@@ -30,10 +48,22 @@ defmodule Servy.Handler do
     """
   end
 
+  defp status_reason(code) do
+    codes = %{
+      200 => "OK",
+      201 => "Created",
+      401 => "Unauthorized",
+      403 => "Forbidden",
+      404 => "Not Found",
+      500 => "Internal Server Error",
+    }
+    codes[code]
+  end
+
 end
 
 
-request = """
+wild_request = """
 GET /wildthings HTTP/1.1
 Host: example.com
 User-Agent: ExampleBrowser/1.0
@@ -41,6 +71,39 @@ Accept: */*
 
 """
 
+wild_response = Servy.Handler.handle(wild_request)
+IO.puts wild_response
 
-response = Servy.Handler.handle(request)
-IO.puts response
+bears_req = """
+GET /bears HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+bears_response = Servy.Handler.handle(bears_req)
+IO.puts bears_response
+
+bears1_req = """
+GET /bears/11 HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+bears1_response = Servy.Handler.handle(bears1_req)
+IO.puts bears1_response
+
+
+bigfoot_req = """
+GET /bigfoot HTTP/1.1
+Host: example.com
+User-Agent: ExampleBrowser/1.0
+Accept: */*
+
+"""
+
+bigfoot_response = Servy.Handler.handle(bigfoot_req)
+IO.puts bigfoot_response
